f558dbd2d0114203c2ff864364fbe7aaa3f5418f
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-14 16:42:53 -0300

    Noções básicas de LifeCycle hooks - ciclo de vida

--- 03_cli-do-vue/projeto_com_cli/src/App.vue
+++ 03_cli-do-vue/projeto_com_cli/src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <PrimeiroComponente />
+  <div>
+    <PrimeiroComponente />
+    <LyfeCycle/>
+  </div>
 </template>
 
 <script>
 //Importando o primeiro componente
   import PrimeiroComponente from './components/PrimeiroComponente.vue'
+  import LyfeCycle from './components/LifeCycle.vue'
 
   export default{
     name: "App",
     components:{
-      PrimeiroComponente
-    }
+      PrimeiroComponente,
+      LyfeCycle
+    },
+    
   }
 </script>
---
+++ 03_cli-do-vue/projeto_com_cli/src/components/LifeCycle.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Meu Nome é: {{ nome }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LyfeCycle",
+  data() {
+    return {
+      nome: "Ainda não sei",
+    };
+  },
+  created() {
+    this.nome = "Anne";
+  },
+  mounted(){
+    this.nome = "Pedro"
+  }
+};
+</script>
