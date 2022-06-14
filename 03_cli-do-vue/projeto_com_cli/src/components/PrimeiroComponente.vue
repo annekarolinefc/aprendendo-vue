74d26102eba219d6667d755c12cf6bdccba5bf52
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-06-14 16:29:17 -0300

    Dados nos componentes

--- 03_cli-do-vue/projeto_com_cli/src/components/PrimeiroComponente.vue
+++ 03_cli-do-vue/projeto_com_cli/src/components/PrimeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
-    <h1>Olá Vue!</h1>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e estudo para ser {{profissao}}.</p>
+    </div>
 </template>
 
 <script>
     export default{
-        name: 'PrimeiroComponente'
+        name: 'PrimeiroComponente',
+        data(){
+            return{
+                nome: "Anne",
+                profissao: "Programadora"
+            }
+        }
     }
 </script>
